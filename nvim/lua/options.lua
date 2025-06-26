766450a8dfd73a1a984e5143b0e5279815b03f34
Alexandru Ciuca <a.ciuca@hotmail.com>
2025-06-26 15:52:19 -0400

    nvim config files

---
+++ nvim/lua/options.lua
@@ -0,0 +1,20 @@
+vim.opt.number = true
+--vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Syncs sys clipboard w nvim's clipboard
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 10
+
+vim.opt.virtualedit = "block"
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
